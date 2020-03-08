0429e48e670975b3e24d8cde206f6b63005d6630
Abdullai Tahiru <42780120+Tachyon18@users.noreply.github.com>
2020-03-08 17:29:46 +0000

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from . forms import SignUpForm
+
+def index(request):
+    return render(request,'base.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
